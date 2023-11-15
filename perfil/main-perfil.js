e06e4243bb226233bbfdd9a8f24315cadde84f43
IIIgorMoura <igor2007moura@gmail.com>
2023-11-15 14:09:03 -0300

    corrigir pos texto menu

--- perfil/main-perfil.js
+++ perfil/main-perfil.js
@@ -1,1 +1,31 @@
-const menuBurguer = document.getElementById("menuBurguer")
+function menuExtendido() {
+    const menuBurger = document.getElementById("menuBurger");
+    const barraDivisao = document.getElementById("barra-divisao");
+
+    const opcoes = document.querySelectorAll(".opcao-menuBurger");
+    const nomeOpcoes = document.querySelectorAll(".opcao-nome");
+
+    if (menuBurger.style.width === "400px") {
+        menuBurger.style.width = "100px";
+        barraDivisao.style.width = "100px";
+
+        opcoes.forEach(opcao => {
+            opcao.style.width = "100px";
+        });
+
+        nomeOpcoes.forEach(nomeOpcao => {
+            nomeOpcao.style.display = "none";
+        });
+    } else {
+        menuBurger.style.width = "400px";
+        barraDivisao.style.width = "400px";
+
+        opcoes.forEach(opcao => {
+            opcao.style.width = "400px";
+        });
+
+        nomeOpcoes.forEach(nomeOpcao => {
+            nomeOpcao.style.display = "inline";
+        });
+    }
+}
