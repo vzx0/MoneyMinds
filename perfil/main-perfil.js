f5d2b6b096900a93641d95c3347e48bbf207fafb
IIIgorMoura <igor2007moura@gmail.com>
2023-11-27 15:29:50 -0300

    dados imagem e nome

--- perfil/main-perfil.js
+++ perfil/main-perfil.js
@@ -34,15 +34,29 @@ function progressoMarketing() {
 }
 
 // funcoes trocar pagina
+alteracaoPerfil = document.getElementById('alteracaoPerfil')
 dashboard = document.getElementById('dashboard');
 cursosDashboard = document.getElementById('cursos');
 ranking = document.getElementById('ranking');
 atividades = document.getElementById('atividades');
 
+function exibirAlteracaoPerfil() {
+    if (alteracaoPerfil.style.display === "flex") {
+        return
+    } else {
+        alteracaoPerfil.style.display = "flex"
+        dashboard.style.display = "none";
+        cursosDashboard.style.display = "none";
+        ranking.style.display = "none";
+        atividades.style.display = "none";
+    }
+}
+
 function exibirDashboard() {
     if (dashboard.style.display === "flex") {
         return
     } else {
+        alteracaoPerfil.style.display = "none"
         dashboard.style.display = "flex";
         cursosDashboard.style.display = "none";
         ranking.style.display = "none";
@@ -53,6 +67,7 @@ function exibirCursos() {
     if (cursosDashboard.style.display === "flex") {
         return
     } else {
+        alteracaoPerfil.style.display = "none"
         dashboard.style.display = "none";
         cursosDashboard.style.display = "flex";
         ranking.style.display = "none";
@@ -63,6 +78,7 @@ function exibirRanking() {
     if (ranking.style.display === "flex") {
         return
     } else {
+        alteracaoPerfil.style.display = "none"
         dashboard.style.display = "none";
         cursosDashboard.style.display = "none";
         ranking.style.display = "flex";
@@ -73,6 +89,7 @@ function exibirAtividades() {
     if (atividades.style.display === "flex") {
         return
     } else {
+        alteracaoPerfil.style.display = "none"
         dashboard.style.display = "none";
         cursosDashboard.style.display = "none";
         ranking.style.display = "none";
@@ -92,8 +109,6 @@ function extenderInformacoes(event) {
     let informacoesCurso = event.currentTarget.querySelector('.informacoesCurso');
     if (informacoesCurso.style.height !== "300px") {
         informacoesCurso.style.height = "300px";
-
-
     }
 }
 
@@ -105,17 +120,121 @@ function retrairInformacoes(event) {
 }
 
 cursos.forEach(curso => {
-    curso.addEventListener("mouseover", function(event) {
+    curso.addEventListener("mouseover", function (event) {
         let informacoesTexto = event.currentTarget.querySelector('.informacoesTexto');
         if (informacoesTexto) {
             informacoesTexto.textContent = "Descubra o poder das finanças e transforme sua relação com o dinheiro neste curso abrangente sobre finanças.";
         }
     });
 
-    curso.addEventListener("mouseleave", function(event) {
+    curso.addEventListener("mouseleave", function (event) {
         let informacoesTexto = event.currentTarget.querySelector('.informacoesTexto');
         if (informacoesTexto) {
             informacoesTexto.textContent = "";
         }
     });
 });
+
+// ---------- PERFIL ----------
+// popup imagem
+popupImagem = document.getElementById('alteracaoPerfil-imagem')
+
+function popupFoto() {
+    popupImagem.style.display = "flex";
+}
+
+function fecharPopupFoto() {
+    popupImagem.style.display = "none";
+}
+
+// gerar foto
+var mediaStream;
+
+function abrirCamera() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(function (stream) {
+            mediaStream = stream;
+            const areaVideo = document.getElementById('camera');
+            areaVideo.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error('Erro ao acessar a câmera', error)
+        })
+};
+
+let downloadLink;
+const perfilImg = document.getElementById('perfil-img');
+const perfilImgPagina = document.getElementById('imagemPerfil');
+
+function tirarFoto() {
+    const areaVideo = document.getElementById('camera');
+    const canvas = document.createElement('canvas');
+
+    canvas.width = areaVideo.videoWidth;
+    canvas.height = areaVideo.videoHeight;
+
+    const context = canvas.getContext('2d');
+    context.drawImage(areaVideo, 0, 0, canvas.width, canvas.height);
+
+    const imageDataURL = canvas.toDataURL();
+    const fotoDiv = document.getElementById('foto');
+
+    fotoDiv.style.backgroundImage = `url(${imageDataURL})`;
+
+    // armazena a foto no localStorage
+    localStorage.setItem('fotoPerfil', imageDataURL);
+
+    // def img do perfil com a foto capturada
+    perfilImg.style.backgroundImage = `url(${imageDataURL})`;
+    perfilImg.style.backgroundSize = "cover";
+
+    perfilImgPagina.style.backgroundImage = `url(${imageDataURL})`;
+    perfilImgPagina.style.backgroundSize = "cover";
+
+    // remove o botão antigo
+    if (downloadLink) {
+        downloadLink.remove();
+    }
+
+    downloadLink = document.createElement('a');
+    downloadLink.href = imageDataURL;
+    downloadLink.download = 'foto.png';
+    downloadLink.textContent = 'Clique para salvar';
+    downloadLink.className = 'botaoPopup botaoPopupMaior';
+
+    document.querySelector('#areaFoto').appendChild(downloadLink);
+}
+
+// Função para verificar e carregar a foto do localStorage ao carregar a página
+document.addEventListener("DOMContentLoaded", function () {
+    const fotoSalva = localStorage.getItem('fotoPerfil');
+
+    if (fotoSalva) {
+        const fotoDiv = document.getElementById('foto');
+        fotoDiv.style.backgroundImage = `url(${fotoSalva})`;
+
+        perfilImg.style.backgroundImage = `url(${fotoSalva})`;
+        perfilImg.style.backgroundSize = "cover";
+
+        perfilImgPagina.style.backgroundImage = `url(${fotoSalva})`;
+        perfilImgPagina.style.backgroundSize = "cover";
+
+        // Cria o botão para baixar a foto caso exista no localStorage
+        downloadLink = document.createElement('a');
+        downloadLink.href = fotoSalva;
+        downloadLink.download = 'foto.png';
+        downloadLink.textContent = 'Clique para salvar';
+        downloadLink.className = 'botaoPopup botaoPopupMaior';
+
+        document.querySelector('#areaFoto').appendChild(downloadLink);
+    }
+});
+
+function trocarNome() {
+    const inputNome = document.getElementById('inputNome').value;
+    const nome = document.getElementById('alterarNome');
+
+    nome.textContent = inputNome;
+}
+
+
