82d17146989ff9396656f3e71cb95e8129c1f16e
IIIgorMoura <igor2007moura@gmail.com>
2023-12-02 16:09:45 -0300

    secao video cursos

--- cursos/investimentoMain.js
+++ cursos/investimentoMain.js
@@ -1,12 +1,21 @@
-const video1 = document.getElementById('investimentoVideo1')
-const botaoVideo1 = document.getElementById('investimentoVideoBotao1')
+// ---------- curso VIDEO 1 ----------
+const video1 = document.getElementById('investimentoVideo1');
+const botaoVideo1 = document.getElementById('investimentoVideoBotao1');
 
-video1.addEventListener('ended', function() {
+video1.addEventListener('ended', function () {
     botaoVideo1.disabled = false;
     botaoVideo1.style.backgroundColor = "#00FF66";
 });
 
-botaoVideo1.addEventListener('click', function() {
-    // Ação a ser executada após o clique no botão, após o término do vídeo
+botaoVideo1.addEventListener('click', function () {
+    
     console.log('Botão clicado após o vídeo!');
+});
+
+// barra de progresso do video no botao
+const barraProgresso = document.getElementById('progresso1');
+
+video1.addEventListener('timeupdate', function () {
+    const progresso = (video1.currentTime / video1.duration) * 100;
+    barraProgresso.style.width = progresso + '%';
 });
