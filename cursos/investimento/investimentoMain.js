4f69cda5523bec0ee0b048fb9b17c12525287023
IIIgorMoura <igor2007moura@gmail.com>
2023-12-02 17:15:33 -0300

    funcionamento capitulos

--- cursos/investimento/investimentoMain.js
+++ cursos/investimento/investimentoMain.js
@@ -1,3 +1,25 @@
+// ---------- menu burger ----------
+
+// ---------- Capitulos ---------- 
+const botoesCapitulo = [];
+const capitulos = [];
+
+for (let i = 1; i <= 3; i++) {
+    botoesCapitulo.push(document.getElementById(`botaoCapitulo${i}`));
+    capitulos.push(document.getElementById(`capitulo${i}`));
+
+    botoesCapitulo[i - 1].addEventListener('click', () => {
+        capitulos.forEach((capitulo, index) => {
+            if (index === i - 1) {
+                capitulo.style.display = 'flex';
+            } else {
+                capitulo.style.display = 'none';
+            }
+        });
+    });
+}
+
+
 // ---------- curso VIDEO 1 ----------
 const video1 = document.getElementById('investimentoVideo1');
 const botaoVideo1 = document.getElementById('investimentoVideoBotao1');
@@ -7,15 +29,15 @@ video1.addEventListener('ended', function () {
     botaoVideo1.style.backgroundColor = "#00FF66";
 });
 
-botaoVideo1.addEventListener('click', function () {
-    
+botaoVideo1.addEventListener('click', () => {
+
     console.log('Botão clicado após o vídeo!');
 });
 
 // barra de progresso do video no botao
 const barraProgresso = document.getElementById('progresso1');
 
-video1.addEventListener('timeupdate', function () {
+video1.addEventListener('timeupdate', () => {
     const progresso = (video1.currentTime / video1.duration) * 100;
     barraProgresso.style.width = progresso + '%';
 });
