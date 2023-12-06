eae79a20bbbec4d1b5ba769b52971d04190951c0
IIIgorMoura <igor2007moura@gmail.com>
2023-12-06 16:48:38 -0300

    questionarios funcionais

--- cursos/mainCursos.js
+++ cursos/mainCursos.js
@@ -509,5 +509,166 @@ function questionario2(){
     exibirPergunta2();
 }
 
+// QUIZ CAP3
+function questionario3(){
+    const perguntas3 = [
+        {
+            pergunta: "A vida faz sentido?",
+            respostas: [
+                { texto: "Não sei", correta: true },
+                { texto: "Neymar?", correta: false },
+                { texto: "Certamente que não", correta: false },
+                { texto: "Certamente que sim", correta: false }
+            ]
+        },
+        {
+            pergunta: "7*7",
+            respostas: [
+                { texto: "190", correta: false },
+                { texto: "27", correta: false },
+                { texto: "14", correta: false },
+                { texto: "49", correta: true }
+            ]
+        },
+        {
+            pergunta: "Você gosta de javascript",
+            respostas: [
+                { texto: "Depende do dia", correta: true },
+                { texto: "Claro", correta: false },
+                { texto: "Não", correta: false },
+                { texto: "Adoro fazer programa", correta: false }
+            ]
+        },
+        {
+            pergunta: "Por que esse site precisa de um coco para funcionar?",
+            respostas: [
+                { texto: "Coco?", correta: false },
+                { texto: "Team Fortress 2", correta: true },
+                { texto: "Sla fi", correta: false },
+                { texto: "Concordo plenamente", correta: false }
+            ]
+        },
+    ];
+
+    function shuffleArray(perguntas3) {
+        for (let i = perguntas3.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [perguntas3[i], perguntas3[j]] = [perguntas3[j], perguntas3[i]];
+        }
+        return perguntas3;
+    }
+
+    const perguntasEmbaralhadas3 = shuffleArray(perguntas3);
+
+    perguntasEmbaralhadas3.forEach(pergunta => {
+        shuffleArray(pergunta.respostas);
+    });
+
+    let perguntaAtual3 = 0;
+    let respostasCorretas3 = 0;
+
+    function exibirPergunta3() {
+        const secaoResposta3 = document.getElementById('secaoResposta3');
+        secaoResposta3.innerHTML = '';
+
+        const pergunta = perguntas3[perguntaAtual3];
+
+        const divPergunta = document.createElement('div');
+        divPergunta.classList.add('pergunta');
+        divPergunta.textContent = pergunta.pergunta;
+
+        const divRespostas = document.createElement('div');
+        divRespostas.classList.add('respostas');
+
+        pergunta.respostas.forEach((resposta, index) => {
+            const divResposta = document.createElement('div');
+            divResposta.classList.add('resposta');
+            divResposta.textContent = resposta.texto;
+            divResposta.dataset.correta = resposta.correta;
+            divResposta.dataset.indice = index;
+
+            divResposta.addEventListener('click', conferirResposta3);
+
+            divRespostas.appendChild(divResposta);
+        });
+
+        divPergunta.appendChild(divRespostas);
+        secaoResposta3.appendChild(divPergunta);
+    }
+
+    function conferirResposta3(event) {
+        const respostaClicada = event.target;
+        const respostaCorreta = respostaClicada.dataset.correta === 'true';
+
+        const conclusaoQuestoes3 = document.getElementById('conclusaoQuestoes3');
+        const tituloFimQuestoes3 = document.getElementById('tituloFimQuestoes3');
+        const textoFimQuestoes3 = document.getElementById('textoFimQuestoes3');
+
+        const botaoFimQuestoesProsseguir3 = document.getElementById('botaoFimQuestoesProsseguir3');
+        const botaoFimQuestoesTentarNovamente3 = document.getElementById('botaoFimQuestoesTentarNovamente3');
+
+        if (respostaCorreta) {
+            respostaClicada.classList.add('correta');
+            respostasCorretas3 += 25;
+        } else {
+            respostaClicada.classList.add('errada');
+            respostaClicada.style.backgroundColor = "red";
+        }
+
+        perguntaAtual3++;
+        if (perguntaAtual3 < perguntas3.length) {
+            setTimeout(exibirPergunta3, 250);
+        }
+
+        // se conseguiu
+        else if (respostasCorretas3 >= 75) {
+            secaoQuestoes3.style.display = "none";
+            conclusaoQuestoes3.style.display = "flex";
+
+            tituloFimQuestoes3.textContent = "Parabéns, você conseguiu!";
+            textoFimQuestoes3.textContent = `Você acertou ${respostasCorretas3}% das questões, clique aqui para ir para a próxima fase: `
+            botaoFimQuestoesProsseguir3.style.display = "flex";
+            botaoFimQuestoesTentarNovamente3.style.display = "none";
+
+            // Adicione uma variável para rastrear o capítulo atual
+            let capituloAtual = 1;
+
+            // No evento de clique do botão "Prosseguir"
+            botaoFimQuestoesProsseguir3.addEventListener('click', () => {
+                // Certifique-se de não ultrapassar o número total de capítulos
+                if (capituloAtual < capitulos.length - 1) {
+                    // Oculta o capítulo atual e exibe o próximo capítulo
+                    capitulos[capituloAtual].style.display = 'none';
+                    capituloAtual++;
+                    capitulos[capituloAtual].style.display = 'flex';
+                } else {
+                    // Se atingir o último capítulo, você pode ocultar a seção de questões ou realizar outra ação
+                    secaoQuestoes3.style.display = 'none';
+                }
+            });
+
+        }
+
+        else {
+            secaoQuestoes3.style.display = "none";
+            conclusaoQuestoes3.style.display = "flex";
+
+            tituloFimQuestoes3.textContent = "Tente novamente!"
+            textoFimQuestoes3.textContent = `Você acertou ${respostasCorretas3}% das questões, clique aqui para tentar novamente: `
+            botaoFimQuestoesTentarNovamente3.style.display = "flex";
+
+            botaoFimQuestoesTentarNovamente3.addEventListener('click', () => {
+                perguntaAtual3 = 0;
+                respostasCorretas3 = 0; // Reinicia as respostas corretas para zero
+                exibirPergunta3(shuffleArray(perguntas3)); // Chama a função para exibir a primeira pergunta
+
+                secaoQuestoes3.style.display = "flex";
+                conclusaoQuestoes3.style.display = "none";
+            });
+        };
+    };
+    exibirPergunta3();
+}
+
 
 
