a2b81f67a79109ab037dea051bd2cfba53a8beaa
IIIgorMoura <igor2007moura@gmail.com>
2023-12-12 04:10:21 -0300

    explicacao JS

--- cursos/mainCursos.js
+++ cursos/mainCursos.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // ---------- menu burger ----------
 // cap1
+// definindo constantes para capturar os elementos
 const opcaoVideo = document.getElementById('opcaoVideo');
 const opcaoTexto = document.getElementById('opcaoTexto');
 const opcaoQuestoes = document.getElementById('opcaoQuestoes');
@@ -24,14 +25,22 @@ const secaoQuestoes = document.getElementById('secaoQuestoes');
 const principal = document.querySelector('.principal');
 
 // abrir e fechar menu mobile
+// seleciona todos elementos com a class "capituloMenu" e armazena em capituloMenus
 const capituloMenus = document.querySelectorAll('.capituloMenu');
+
 const menuMobile = document.getElementById('menuMobile');
 const escurecimentoFundo = document.getElementById('escurecimentoFundo');
 
+// funcoes onclick para abrir e fechar menu
 function abrirMenuMobile() {
     escurecimentoFundo.style.display = 'flex';
+
+    // após 50 milissegundos a funcao é executada
     setTimeout(() => {
+        // tornando o escurecimentoFundo visivel através da opacidade
         escurecimentoFundo.style.opacity = '1';
+
+        // movendo o menuMobile para a tela, fazendo com que o o mesmo fique a 0px de distancia do lado direito da página
         menuMobile.style.right = '0';
     }, 50);
     menuMobile.style.display = 'flex';
@@ -50,11 +59,12 @@ function fecharMenuMobile(){
 
 
 
-// Verifica se está no modo mobile
+// Verifica se está no modo mobile ou original/desktop
 const mobile = window.matchMedia('(max-width: 400px)').matches;
 const original = window.matchMedia('min-width: 401px').matches;
 
 if (mobile) {
+    // loop forEach que percorre cada elemento no array capituloMenus
     capituloMenus.forEach(capituloMenu => {
         // Move cada elemento capituloMenu para o menuMobile
         menuMobile.appendChild(capituloMenu);
@@ -66,12 +76,12 @@ const menuDesktop = document.getElementById('menuDesktop');
 
 if (original) {
     capituloMenus.forEach(capituloMenu => {
-        // Move cada elemento capituloMenu para o menuMobile
+        // Move cada elemento capituloMenu para o menuDesktop
         menuDesktop.appendChild(capituloMenu);
     });
 }
 
-
+// ao clicar na opcao, a mesma executa uma funcao que oculta as demais secoes da página e exibe a pagina correspondente ao botao
 opcaoVideo.addEventListener('click', () => {
     ocultarConteudos();
     secaoVideo.style.display = 'flex';
@@ -172,15 +182,21 @@ function ocultarConteudos3() {
 }
 
 // ---------- Capitulos ---------- 
+// declaracao de arrays para botoesCapitulo e capitulos
 const botoesCapitulo = [];
 const capitulos = [];
 
+// loop for itera até o numero total de capitulos (3)
 for (let i = 1; i <= 3; i++) {
+
+    // envia para o array botoesCapitulo, o elemento correspondente ao id resultante da concatenacao: botaoCapitulo e o valor de i
     botoesCapitulo.push(document.getElementById(`botaoCapitulo${i}`));
     capitulos.push(document.getElementById(`capitulo${i}`));
 
+    // acessa cada botao especifico do array
     botoesCapitulo[i - 1].addEventListener('click', () => {
         capitulos.forEach((capitulo, index) => {
+            // se o indice do capitulo for igual a i - 1
             if (index === i - 1) {
                 capitulo.style.display = 'flex';
             } else {
@@ -192,6 +208,7 @@ for (let i = 1; i <= 3; i++) {
 
 
 // ---------- curso VIDEO ----------
+// arrays com elementos de cada secao
 const videos = [
     document.getElementById('video1'),
     document.getElementById('video2'),
@@ -211,9 +228,12 @@ const barrasProgresso = [
 ];
 
 videos.forEach((video, index) => {
+    // quando o video termina, o botao com index correspondente é ativado
     video.addEventListener('ended', function () {
+        // tornando falsa, a tag disabled
         botoes[index].disabled = false;
         botoes[index].style.backgroundColor = "var(--corBotaoDestaque)";
+        // alterando o icone do cursor
         botoes[index].style.cursor = "pointer";
     });
 
@@ -223,11 +243,16 @@ videos.forEach((video, index) => {
         principal.style.height = '100%';
         opcaoTexto.disabled = "false"
 
+        // remove a classe desativado
         opcaoTexto.classList.remove('desativado');
     });
 
+    // funcao para a progressao da barra dentro do botao "Prosseguir"
     video.addEventListener('timeupdate', () => {
+        // constante progresso extrai o momento atual do video, dividindo pela duracao extraida do video e multiplicando por 100
         const progresso = (video.currentTime / video.duration) * 100;
+
+        // a barra de progresso correspondente recebe a largura equivalente a % de progresso do video
         barrasProgresso[index].style.width = progresso + '%';
     });
 });
